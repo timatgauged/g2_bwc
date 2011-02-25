05d4e9b844685aa5cdd8977ed8f9a11f7fd69644
Timothy Robbins <timothyrobbins@new-host.home>
2011-02-25 10:42:27 -0500

    site files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluewolf::Application.config.secret_token = '7d41e5c6f7b2e4d195581e54d48ab3c03fc3e790c7c8ed3c86e2500a4878ce435f6a080bba110a154c119ab535ba823b4ef4b9b4c635719af5ac26a7e236db25'
